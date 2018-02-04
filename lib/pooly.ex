227d794ecef7d35e9caafab20733b2c6d3faca94
Matt Thompson <mthompson@eltoro.com>
2018-02-04 13:54:29 -0500

    notes

--- lib/pooly.ex
+++ lib/pooly.ex
@@ -1,14 +1,28 @@
 defmodule Pooly do
   use Application
 
-  # When Pooly is started -- start/2 -- calls start/1 for convenience
-  # max_overflow 
+  @timeout 5000
+
   def start(_type, _args) do
-    pools_config = [
-      [name: "Pool1", mfa: {SampleWorker, :start_link, []}, size: 2, max_overflow: 3],
-      [name: "Pool2", mfa: {SampleWorker, :start_link, []}, size: 3, max_overflow: 0],
-      [name: "Pool3", mfa: {SampleWorker, :start_link, []}, size: 4, max_overflow: 0],
-    ]
+    pools_config =
+      [
+        [name: "Pool1",
+         mfa: {SampleWorker, :start_link, []},
+         size: 2,
+         max_overflow: 1
+        ],
+        [name: "Pool2",
+         mfa: {SampleWorker, :start_link, []},
+         size: 3,
+         max_overflow: 0
+        ],
+        [name: "Pool3",
+         mfa: {SampleWorker, :start_link, []},
+         size: 4,
+         max_overflow: 0
+        ],
+      ]
+
     start_pools(pools_config)
   end
 
@@ -16,15 +30,25 @@ defmodule Pooly do
     Pooly.Supervisor.start_link(pools_config)
   end
 
-  def checkout(pool_name) do
-    Pooly.Server.checkout(pool_name)
+  def checkout(pool_name, block \\ true, timeout \\ @timeout) do
+    Pooly.Server.checkout(pool_name, block, timeout)
   end
 
   def checkin(pool_name, worker_pid) do
     Pooly.Server.checkin(pool_name, worker_pid)
   end
 
+  def transaction(pool_name, fun, timeout) do
+    Pooly.Server.transaction(pool_name, fun, timeout)
+  end
+
   def status(pool_name) do
     Pooly.Server.status(pool_name)
   end
 end
+
+# w1 = Pooly.checkout("Pool1")
+# w2 = Pooly.checkout("Pool2")
+# w3 = Pooly.checkout("Pool3")
+# Pooly.checkout("Pool1", true, :infinity)
+# Pooly.checkout("Pool1", true, 5000) 
